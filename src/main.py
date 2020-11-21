edc134e8b5730296f0b064f1ab01599ee9a9feb6
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-11-21 22:59:48 +0100

    feat: added endpoints

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get('/')
 def ping():
     return {'result': 'Service is alive...'}
 
 
+@app.get('/example')
 async def example_endpoint():
     return {'result': 'Example'}
